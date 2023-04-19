070bdb252a889c9db8169e00f2786bf95697720d
Sergey Stoyan <s.y.stoyan@gmail.com>
2023-04-19 21:45:48 +0300

    3.2.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.2.4.*")]
+[assembly: AssemblyVersion("3.2.5.*")]
 //[assembly: AssemblyFileVersion("1.1.0.0")]
